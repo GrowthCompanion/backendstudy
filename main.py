d1ecb1c83d5c668ab5b70168c0003567f00089fe
GrowthCompanion <147545952+GrowthCompanion@users.noreply.github.com>
2025-02-23 16:19:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -120,5 +120,5 @@ def generate_quiz():
 
 # Run the Flask app
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
